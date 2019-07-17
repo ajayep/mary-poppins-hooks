a1051d5ccd3f4bae71926ba9dee60b580d61a7fa
ajayep <ajay@actiotech.com>
2019-07-17 13:20:24 +0530

    useEffect
    useEffect usage

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import ReactDOM from "react-dom";
 import Row from "./row";
-import { ThemeContext, LocaleContext } from "./context";
+import { ThemeContext } from "./context";
 import "./styles.css";
 import "./greeting.css";
 
@@ -9,6 +9,7 @@ function Greeting(props) {
   const [name, setName] = useState("Mary");
   const [surname, setSurname] = useState("Poppins");
   const theme = useContext(ThemeContext);
+  const width = useWindowWidth();
 
   function handleNameChange(e) {
     setName(e.target.value);
@@ -26,9 +27,23 @@ function Greeting(props) {
       <Row label="Surname">
         <input value={surname} onChange={handleSurnameChange} />
       </Row>
+      <Row label="Width">
+        <input value={width} />
+      </Row>
     </section>
   );
 }
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<Greeting />, rootElement);
+
+function useWindowWidth() {
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => setWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  });
+  return width;
+}
